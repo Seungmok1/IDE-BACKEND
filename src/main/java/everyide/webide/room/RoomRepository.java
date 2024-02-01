88dcef08012e84c99aa5c4020a73a05d5a7ed629
Seungmok1 <kww4215@naver.com>
2024-02-01 18:22:31 +0900

    refactor: RoomRepository 위치 변경

--- src/main/java/everyide/webide/room/RoomRepository.java
+++ src/main/java/everyide/webide/room/RoomRepository.java
@@ -1,5 +1,6 @@
-package everyide.webide.room.domain;
+package everyide.webide.room;
 
+import everyide.webide.room.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
