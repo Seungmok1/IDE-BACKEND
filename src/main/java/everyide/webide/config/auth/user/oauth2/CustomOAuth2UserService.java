0d288ade4365fe724ea63c8dd2ba7feaf897bf65
Hyejin724 <148074385+Hyejin724@users.noreply.github.com>
2024-02-01 11:30:13 +0900

    EI-29 UserDetails

--- src/main/java/everyide/webide/config/auth/user/oauth2/CustomOAuth2UserService.java
+++ src/main/java/everyide/webide/config/auth/user/oauth2/CustomOAuth2UserService.java
@@ -1,32 +1,4 @@
 package everyide.webide.config.auth.user.oauth2;
 
-import everyide.webide.user.UserRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
-
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-    private final UserRepository userRepository;
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-
-        log.info("OAuth 중");
-    }
-
-    private OAuth2User processOAuth2User(OAuth2User oAuth2User, OAuth2UserRequest oAuth2UserRequest) {
-
-    }
-
-
-
-
+public class CustomOAuth2UserService {
 }
-
