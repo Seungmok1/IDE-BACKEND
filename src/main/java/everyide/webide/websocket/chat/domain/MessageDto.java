eb673c476569463482d52b68da8ddcd3cdfa5868
Hyejin724 <imhaejin98@gmail.com>
2024-02-04 20:29:07 +0900

    updatepassword_getmapping

--- src/main/java/everyide/webide/websocket/chat/domain/MessageDto.java
+++ src/main/java/everyide/webide/websocket/chat/domain/MessageDto.java
@@ -1,13 +1,13 @@
-//package everyide.webide.websocket.chat.domain;
-//
-//import lombok.Getter;
-//
-//import java.io.Serializable;
-//
-//@Getter
-//public class MessageDto implements Serializable {
-//    private String roomId;
-//    private String contentType;
-//    private String content;
-//    private Long senderId;
-//}
+package everyide.webide.websocket.chat.domain;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class MessageDto implements Serializable {
+    private String roomId;
+    private String contentType;
+    private String content;
+    private Long senderId;
+}
