3f56151e8e45cb0012bb1a27a0790b2b7f9a879d
Hyejin724 <148074385+Hyejin724@users.noreply.github.com>
2024-02-06 12:58:59 +0900

    fix url /user/info

--- src/main/java/everyide/webide/websocket/kafka/KafkaConsumerConfig.java
+++ src/main/java/everyide/webide/websocket/kafka/KafkaConsumerConfig.java
@@ -1,39 +1,39 @@
-//package everyide.webide.websocket.kafka;
-//
-//import org.apache.kafka.clients.consumer.ConsumerConfig;
-//import org.apache.kafka.common.serialization.StringDeserializer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-//import org.springframework.kafka.core.ConsumerFactory;
-//import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-//import org.springframework.kafka.support.serializer.JsonDeserializer;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-////@Configuration
-//public class KafkaConsumerConfig {
-//
-//    @Bean
-//    public ConsumerFactory<String, Object> consumerFactory(){
-//
-//        Map<String, Object> config = new HashMap<>();
-//        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaProperties.BROKER);
-//        config.put(ConsumerConfig.GROUP_ID_CONFIG, KafkaProperties.GROUP_ID);
-//        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-//        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-//        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-//
-//        return new DefaultKafkaConsumerFactory<>(config);
-//    }
-//
-//    @Bean
-//    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-//        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-//        factory.setConsumerFactory(consumerFactory());
-//
-//        return factory;
-//    }
-//}
+package everyide.webide.websocket.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory(){
+
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaProperties.BROKER);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, KafkaProperties.GROUP_ID);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
+    }
+}
