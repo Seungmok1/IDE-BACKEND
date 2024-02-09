8ea15803c08ba8f8ef750dad6a0259237ddef0f4
JamieJay <jwjh1205@naver.com>
2024-02-09 19:50:16 +0900

    Create init.sql

---
+++ k8s/configs/init.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS `krampoline` DEFAULT CHARACTER SET utf8mb4;
+
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
+GRANT ALL ON krampoline.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
+
+USE `krampoline`;
+
+DROP TABLE IF EXISTS `everyide`;
+CREATE TABLE `everyide`;
