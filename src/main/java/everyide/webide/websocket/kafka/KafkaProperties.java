ee881fa1e8c185e98a400b33ee65436a54040bfd
JamieJay <jwjh1205@naver.com>
2024-02-12 20:57:44 +0900

    Update KafkaProperties.java

--- src/main/java/everyide/webide/websocket/kafka/KafkaProperties.java
+++ src/main/java/everyide/webide/websocket/kafka/KafkaProperties.java
@@ -6,5 +6,5 @@ public final class KafkaProperties {
     public static String uuid = UUID.randomUUID().toString();
     public static final String CHAT_TOPIC = "chat";
     public static final String GROUP_ID = uuid;
-    public static final String BROKER = "kafka:9092";
+    public static final String BROKER = "localhost:9092";
 }
