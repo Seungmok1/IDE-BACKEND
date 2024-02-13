b9e22e1e84503bf1aa2309633cba12187c25d0d7
Seungmok1 <kww4215@naver.com>
2024-02-13 12:02:10 +0900

    remove: 카프카 삭제

--- src/main/java/everyide/webide/websocket/CustomHandshakeHandler.java
+++ src/main/java/everyide/webide/websocket/CustomHandshakeHandler.java
@@ -15,7 +15,6 @@ public class CustomHandshakeHandler extends DefaultHandshakeHandler {
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
         String sessionId = UUID.randomUUID().toString();
         attributes.put("SessionId", sessionId);
-
         return new Principal() {
             @Override
             public String getName() {
--- src/main/java/everyide/webide/websocket/chat/MessageController.java
+++ src/main/java/everyide/webide/websocket/chat/MessageController.java
@@ -2,13 +2,10 @@ package everyide.webide.websocket.chat;
 
 import everyide.webide.websocket.chat.domain.Message;
 import everyide.webide.websocket.chat.domain.MessageDto;
-import everyide.webide.websocket.kafka.KafkaProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,13 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MessageController {
 
-    private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final SimpMessagingTemplate simpMessagingTemplate;
     private final MessageRepository messageRepository;
 
     @Transactional
     @MessageMapping("/room/{roomId}/chat")
-    public void message(MessageDto messageDto, @DestinationVariable String roomId) {
+    @SendTo("/topic/room/{roomId}/chat")
+    public MessageDto message(MessageDto messageDto, @DestinationVariable String roomId) {
         Message message = Message.builder()
                 .roomId(roomId)
                 .contentType(messageDto.getContentType())
@@ -32,11 +28,6 @@ public class MessageController {
                 .senderId(messageDto.getSenderId())
                 .build();
         messageRepository.save(message);
-        kafkaTemplate.send(KafkaProperties.CHAT_TOPIC, messageDto);
-    }
-
-    @KafkaListener(topics = KafkaProperties.CHAT_TOPIC)
-    public void receive(MessageDto messageDto) {
-        simpMessagingTemplate.convertAndSend("/topic/room/" + messageDto.getRoomId() + "/chat", messageDto);
+        return messageDto;
     }
 }
